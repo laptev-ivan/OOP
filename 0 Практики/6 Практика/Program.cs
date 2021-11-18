dd11d6d526ea7533df5e8e8a72813e7b70170912
LaptevIvan <laptev.ivan@lit1533.ru>
2021-11-18 12:24:05 +0300

    Commit

---
+++ 0 Практики/6 Практика/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _6_Практика
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint a = 9;
+            uint b = 2;
+            Console.WriteLine($"Побитовое \"И\":\t{a & b}");
+            Console.WriteLine($"Побитовое \"ИЛИ\":\t{a|b}");
+            Console.WriteLine($"Побитовое \"исключающее ИЛИ\":\t{a^b}");
+            Console.WriteLine($"Инверсия a:\t{~a}");
+            Console.WriteLine($"Инверсия b:\t{~b}");
+            Console.WriteLine($"Побитовый сдвиг влево a на 1 бит:\t{a<<1}");
+            Console.WriteLine($"Побитовый сдвиг вправо b на 2 бит:\t{b>>2}");
+            Console.ReadKey();
+        }
+    }
+}
