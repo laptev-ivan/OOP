20afd506289a8602513542d86611c67b0b00c6d8
LaptevIvan <laptev.ivan@lit1533.ru>
2021-11-21 23:33:59 +0300

    Commit

---
+++ Основы языка CS/Побитовые операции/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Побитовые_операции
+{
+    class Program
+    {
+        static int masking(ushort x1, int i)
+        {
+            int mask = 1, count = 0;
+            mask <<= i;
+            if ((x1 & mask) != 0)
+                return 1;
+            else
+                return 0;
+        }
+
+        static int masking1(short x2, int i)
+        {
+            int mask = 1;
+            mask <<= i;
+            if ((x2 & mask) != 0)
+                return 1;
+            else
+                return 0;
+        }
+
+        static void binary_code(ushort x1)
+        {
+            int count = 0;
+            for (int i = 15; i >= 0; --i)
+            {
+                if (masking(x1, i) == 0)
+                    count++;
+            }
+            Console.WriteLine($"Кол-во нулевых бит числа x1: {count}");
+        }
+
+        static void no_zeros(short x2)
+        {
+            for (int i = 15; i >= 0; --i)
+            {
+                while (masking1(x2, i) != 0)
+                {
+                    Console.Write($"{masking1(x2, i)}");
+                }
+            }   
+        }
+
+        static void bit_mask(ushort x1, ushort n)
+        {
+
+            x1 = (x1 >> n) | (x1 << (16 - n));
+            Console.WriteLine($"Циклический сдвиг на {n} число x1: {x1}");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x1:\t"); ushort x1 = ushort.Parse(Console.ReadLine());
+            Console.WriteLine("Введите n:\t"); ushort n = ushort.Parse(Console.ReadLine());
+            Console.WriteLine("Введите x2:\t"); short x2 = short.Parse(Console.ReadLine());
+            binary_code(x1);
+            no_zeros(x2);
+            bit_mask(x1, n);
+            Console.ReadKey();
+        }
+    }
+}
