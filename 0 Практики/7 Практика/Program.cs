2041d792846345cd77fd73c2b487acc64d8019d3
Ivan Laptev <chelsea09021999@gmail.com>
2021-11-22 15:50:54 +0300

    Commit

---
+++ 0 Практики/7 Практика/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _7_Практика
+{
+    class Program
+    {
+        static void sumOfDigits(ref int a, ref int b)
+        {
+            int copy_a = a;
+            a = a + b;
+            b = copy_a - b;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение a: "); int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите значение b: "); int b = int.Parse(Console.ReadLine());
+            sumOfDigits(ref a, ref b);
+            Console.WriteLine($"Значение a: {a}");
+            Console.WriteLine($"Значение b: {b}");
+            Console.ReadKey();
+        }
+    }
+}
