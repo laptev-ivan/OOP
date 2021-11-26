7c18f6c63b103a16227dc8bb298d59db169a9b5f
Ivan Laptev <chelsea09021999@gmail.com>
2021-11-26 14:41:51 +0300

    Commit

--- 0 Практики/7 Практика/Program.cs
+++ 0 Практики/7 Практика/Program.cs
@@ -4,16 +4,16 @@ namespace _7_Практика
 {
     class Program
     {
-        static void incPar(ref int x1, ref int x2)
+        static void incPar(ref int x1, int x2)
         {
-            double cnt = 0f;
+            int cnt = 0;
             while (x2 > 9)
             {
                 x2 /= 10;
                 ++cnt;
             }
             for (int i = 0; i < cnt; ++i)
-                x1 *= 10;
+                x1 *= 10; 
         }
         static void divPar(ref int x1)
         {
@@ -28,11 +28,11 @@ namespace _7_Практика
         {
             Console.Write("Введите первый параметр: "); int a = int.Parse(Console.ReadLine());
             Console.Write("Введите второй параметр: "); int b = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите c: "); int c = int.Parse(Console.ReadLine());
-            incPar(ref a, ref b);
+            Console.Write("Введите параметр для второй задачи: "); int c = int.Parse(Console.ReadLine());
+            incPar(ref a, b);
             divPar(ref c);
-            Console.WriteLine(a);
-            Console.WriteLine(c);
+            Console.WriteLine($"Первое число: {a}");
+            Console.WriteLine($"Второе число: {c}");
             Console.ReadKey(); 
         }
     }
