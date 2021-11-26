2510c653d65f67ac872c888905554cf5bc31f3a4
Ivan Laptev <chelsea09021999@gmail.com>
2021-11-26 13:31:06 +0300

    Commit

--- 0 Практики/7 Практика/Program.cs
+++ 0 Практики/7 Практика/Program.cs
@@ -4,21 +4,36 @@ namespace _7_Практика
 {
     class Program
     {
-        static void sumOfDigits(ref int a, ref int b)
+        static void incPar(ref int x1, ref int x2)
         {
-            int copy_a = a;
-            a = a + b;
-            b = copy_a - b;
+            double cnt = 0f;
+            while (x2 > 9)
+            {
+                x2 /= 10;
+                ++cnt;
+            }
+            for (int i = 0; i < cnt; ++i)
+                x1 *= 10;
         }
-
+        static void divPar(ref int x1)
+        {
+            int del = 1;
+            for (int i = 1; i < x1; ++i)
+                if (x1 % i == 0)
+                    if (i > del)
+                        del = i;
+            x1 = del;
+        }
         static void Main(string[] args)
         {
-            Console.Write("Введите значение a: "); int a = int.Parse(Console.ReadLine());
-            Console.Write("Введите значение b: "); int b = int.Parse(Console.ReadLine());
-            sumOfDigits(ref a, ref b);
-            Console.WriteLine($"Значение a: {a}");
-            Console.WriteLine($"Значение b: {b}");
-            Console.ReadKey();
+            Console.Write("Введите первый параметр: "); int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите второй параметр: "); int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите c: "); int c = int.Parse(Console.ReadLine());
+            incPar(ref a, ref b);
+            divPar(ref c);
+            Console.WriteLine(a);
+            Console.WriteLine(c);
+            Console.ReadKey(); 
         }
     }
 }
