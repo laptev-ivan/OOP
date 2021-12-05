811c01de7b96bc6d2e71215fd56549a27ee1af46
LaptevIvan <laptev.ivan@lit1533.ru>
2021-12-06 00:28:36 +0300

    Commit

--- 0 Практики/Dwarfs/Program.cs
+++ 0 Практики/Dwarfs/Program.cs
@@ -6,36 +6,48 @@ namespace Dwarfs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите число гномов (16 или 32)");
+
+            int order2, hint = 0, myHat=0, cnt = 0;
+            short order1;
+            Console.WriteLine("Введите число гномов (16 или 32):");
             int numOfDwarfs = int.Parse(Console.ReadLine());
             while(!(numOfDwarfs==16||numOfDwarfs==32))
             {
-                Console.WriteLine("Ошибка: введите число гномов (16 или 32)");
+                Console.WriteLine("Ошибка: введенное число должно быть равно 16 или 32. Повторите ввод:");
                 numOfDwarfs = int.Parse(Console.ReadLine());
             }
-            int cnt = 0;
-            short order1;
-            int order2;
-            int hint = 0;
-            int myHat = 0;
             if(numOfDwarfs==16)
             {
-                Console.WriteLine("Введите число, задающее представление гномов");
+                Console.WriteLine("Введите число, двоичный код которого представляет гномов:");
                 order1 = short.Parse(Console.ReadLine());
-                Console.Write("Представление гномов - ");
+                Console.WriteLine("Гномы:");
                 for(int i=0; i<16; ++i)
                 {
                     if ((order1 & (1 << i)) == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
                         Console.Write("0");
+                        Console.ResetColor();
+                    }
                     else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.Write("1");
+                        Console.ResetColor();
+                    }
                 }
-                Console.WriteLine();
+                Console.WriteLine(); 
                 if ((order1 & 2) == 0)
-                    Console.WriteLine("Первый гном сказал. На мне зеленая шапка если честно  нам без разницы, что говорит первый гном, он просто...");
+                {
+                    Console.WriteLine("1-й гном сказал: \"Я в зеленой шапке.\"");
+                    Console.WriteLine($"1-й гном ошибся и не получил ни одной монеты. Общий счет гномов: {cnt}");
+                }
                 else
-                    Console.WriteLine("Первый гном сказал. На мне красная шапка ...");
-                for(int i=1; i<16; ++i)
+                {
+                    Console.WriteLine("1-й гном сказал: \"Я в красной шапке.\"");
+                    Console.WriteLine($"1-й гном ошибся и не получил ни одной монеты. Общий счет гномов: {cnt}");
+                }
+                for (int i=1; i<16; ++i)
                 {
                     if ((order1 & (1 << i + 1)) == 0)
                         hint = 0;
@@ -49,23 +61,92 @@ namespace Dwarfs
                     if(hint!=myHat)
                     {
                         if (myHat == 0)
-                            Console.WriteLine($"{i + 1} - й гном сказал. На мне зеленая шапка и угадал, общий счет - {cnt}");
+                        {
+                            Console.WriteLine($"{i + 1}-й гном сказал: \"Я в зеленой шапке.\"");
+                            Console.WriteLine($"{i + 1}-й гном угадал и получил монету. Общий подсчет монет: {cnt}");
+                        }
                         else
-                            Console.WriteLine($"{i+1} - й гном сказал. На мне красная шапка и угадал, общий счет - {cnt}");
+                        {
+                            Console.WriteLine($"{i + 1}-й гном сказал: \"Я в красной шапке.\"");
+                            Console.WriteLine($"{i + 1}-й гном угадал и получил монету. Общий подсчет монет: {cnt}");
+                        }
                     }
                 }
             }
             else
             {
-                Console.WriteLine("Ввдетие число, задающее представление гномов");
+                Console.WriteLine("Введите число, двоичный код которого представляет гномов:");
                 order2 = int.Parse(Console.ReadLine());
-                Console.Write("Представление гномов - ");
-                for(int i=0; i<32; ++i)
+                Console.WriteLine("Гномы:");
+                for (int i = 0; i < 32; ++i)
                 {
                     if ((order2 & (1 << i)) == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
                         Console.Write("0");
+                        Console.ResetColor();
+                    }
                     else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.Write("1");
+                        Console.ResetColor();
+                    }
+                }
+                Console.WriteLine();
+                if ((order2 & 2) == 0)
+                {
+                    Console.WriteLine("1-й гном сказал: \"Я в зеленой шапке.\"");
+                    Console.WriteLine($"1-й гном ошибся и не получил ни одной монеты. Общий счет гномов: {cnt}");
+                }
+                else
+                {
+                    Console.WriteLine("1-й гном сказал: \"Я в красной шапке.\"");
+                    Console.WriteLine($"1-й гном ошибся и не получил ни одной монеты. Общий счет гномов: {cnt}");
+                }
+                for (int i = 1; i < 32; i++)
+                {
+
+                    if ((order2 & (1 << i + 1)) == 0)
+                    {
+                        hint = 0;
+                    }
+                    else
+                    {
+                        hint = 1;
+                    }
+                    if ((order2 & (1 << i)) == 0)
+                    {
+                        myHat = 0;
+                    }
+                    else
+                    {
+                        myHat = 1;
+                    }
+                    cnt++;
+                    if (hint != myHat)
+                    {
+                        if (myHat == 0)
+                        {
+                            Console.WriteLine($"{i + 1}-й гном сказал: \"Я в зеленой шапке.\"");
+                            Console.WriteLine($"{i + 1}-й гном угадал и получил монету. Общий подсчет монет: {cnt}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{i + 1}-й гном сказал: \"Я в красной шапке.\"");
+                            Console.WriteLine($"{i + 1}-й гном угадал и получил монету. Общий подсчет монет: {cnt}");
+                        }
+                    }
+                    else if (myHat == 0)
+                    {
+                        Console.WriteLine($"{i + 1}-й гном сказал: \"Я в зеленой шапке и угадал.\"");
+                        Console.WriteLine($"{i + 1}-й гном угадал и получил монету. Общий подсчет монет: {cnt}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{i + 1}-й гном сказал: \"Я в красной шапке и угадал.\"");
+                        Console.WriteLine($"{i + 1}-й гном угадал и получил монету. Общий подсчет монет: {cnt}");
+                    }
                 }
             }
             Console.ReadKey();
