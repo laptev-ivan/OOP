82dac0c13d9d03c7ba1a4880a4c6487a21e79ce7
LaptevIvan <laptev.ivan@lit1533.ru>
2021-12-12 15:41:08 +0300

    Commit

---
+++ Основы языка CS/Редактор текста/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Редактор_текста
+{
+    class Program
+    {   
+        static int CountWords(string s)
+        {
+            int cnt = 1;
+            char[] arr = s.ToCharArray();
+            for(int i=0; i<arr.Length; ++i)
+            {
+                if (arr[i] == ' ') 
+                {
+                    ++cnt;
+                }
+            }
+            return cnt;
+        }
+
+        static string MinWord(string s)
+        {
+            char[] separator = { ' ', '.', ',', '!', '?', ';', ':' };
+            string[] words = s.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            string min = words[0];
+            for (int i=0; i<words.Length; ++i)
+            {
+                if(words[i].Length<min.Length)
+                {
+                    min = words[i];
+                }
+            }
+            return min;
+        }
+
+        static string MaxWord(string s)
+        {
+            char[] separator = { ' ', '.', ',', '!', '?', ';', ':' };
+            string[] words = s.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            string max = words[0];
+            for (int i = 0; i < words.Length; ++i)
+            {
+                if (words[i].Length > max.Length)
+                {
+                    max = words[i];
+                }
+            }
+            return max;
+        }
+
+        static int CountSents(string s)
+        {
+            int cnt = 0;
+            char[] seps = { '.', '!', '?' };
+            char[] arr = s.ToCharArray();
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                if (arr[i] == seps[0] || arr[i] == seps[1] || arr[i] == seps[2])
+                {
+                    ++cnt;
+                }
+            }
+            return cnt;
+        }
+        static string CharsInString(string s)
+        {
+            bool flag = true;
+            string ans = "";
+            s = s.ToLower();
+            for(int i=0; i<s.Length; ++i)
+            {
+                flag = true;
+                for (int j = 0; j < ans.Length; ++j) 
+                {
+                    if (s[i] == ans[j])
+                    {
+                        flag = false;
+                    }
+                }
+                if(flag)
+                {
+                    ans = String.Concat(ans, s[i]);
+                }
+            }
+            return ans;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: "); string s = Console.ReadLine();
+            int SumOfWords = CountWords(s);
+            string MinLengthWord = MinWord(s);
+            string MaxLengthWord = MaxWord(s);
+            int SumOfSentences = CountSents(s);
+            string Chars = CharsInString(s);
+            Console.WriteLine($"Количество слов: {SumOfWords}");
+            Console.WriteLine($"Самое короткое слово: {MinLengthWord}");
+            Console.WriteLine($"Самое длинное слово: {MaxLengthWord}");
+            Console.WriteLine($"Количество предложений: {SumOfSentences}");
+            Console.WriteLine($"Символы: {Chars}");
+            Console.ReadKey();
+        }
+    }
+}
