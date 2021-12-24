c0ac37f77fffe2038c4a42e634bd55b60c0dc401
Ivan Laptev <chelsea09021999@gmail.com>
2021-12-24 14:38:58 +0300

    Commit

---
+++ Основы языка CS/Пила/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Пила
+{
+    class Program
+    {
+        static int KitKat(int n)
+        {
+            int cnt = 0;
+            int[] arr = new int[n];
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            for(int i=2; i<arr.Length-2; ++i)
+            {
+                if((arr[i]>arr[i-1]&&arr[i]>arr[i+1])||(arr[i]<arr[i-1]&&arr[i]<arr[i+1]))
+                {
+                    Console.Write($"{arr[i]} ");
+                    cnt++;
+                }
+            }   
+            return arr.Length - cnt;
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = KitKat(n);
+            Console.WriteLine($"Необходимо вычеркнуть {num} элемента");
+            Console.ReadKey();
+        }
+    }
+}
