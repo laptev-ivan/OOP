3dad4d78ddb34730f16381071ab4efc3bb18a913
Ivan Laptev <chelsea09021999@gmail.com>
2022-01-31 11:03:20 +0300

    Commit

--- 0 Практики/1 Практика/Program.cs
+++ 0 Практики/1 Практика/Program.cs
@@ -1,41 +1,67 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace _1_практика
+namespace _11практика
 {
+    enum Months
+    {
+        Январь = 1,
+        Февраль,
+        Март,
+        Апрель,
+        Май,
+        Июнь,
+        Июль,
+        Август,
+        Сентябрь,
+        Октябрь,
+        Ноябрь,
+        Декабрь,
+    }
+    enum Seasons
+    {
+        Зима, Весна, Лето, Осень
+    }
     class Program
     {
+        static void Info(int[] arr)
+        {
+            for (Months i = Months.Январь; i <= Months.Декабрь; i++)
+            {
+                Console.WriteLine($"Месяц:{i}, номер:{(int)i}, количество дней:{arr[(int)i - 1]}");
+            }
+        }
+        static void SFiguration(int N, int n, int d, int[] NoD)
+        {
+
+            int r = NoD[N - 1];
+            NoD[N - 1] -= n;
+            for (int i = N - 1; i < i + 1; i++)
+            {
+                if (i >= NoD.Length) i = 0;
+                if (d >= NoD[i])
+                {
+                    d -= NoD[i];
+                    NoD[N - 1] = r;
+                }
+                else
+                {
+                    Console.WriteLine($"{(Seasons)((i % 12) / 3)}!");
+                    break;
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.Unicode;
-
-            string s = "AMD";
-            float f1 = 3.20000f;
-            int p = 16;
-            float p1 = 0.78932597f;
-            float s1 = 4f;
-            double r = 64.100d;
-            float pr = 100000000000f;
-            decimal dec = 18500.5m;
-
-            Console.Write("Введите ваше имя: ");
-            string name = Console.ReadLine();
-            Console.Write("Введите дробное число: ");
-            float f = float.Parse(Console.ReadLine());
-            Console.WriteLine();
-            Console.WriteLine($"Привет, {name}!");
-            Console.WriteLine("*********************************\n" +
-                              "*\tЯ твой компьютер!\t*\n" +
-                              "*********************************\n\n" +
-                              "У меня следующие характеристики:\n" + 
-                              $"Процессор\t{s} с разрядностью {f1:F2}GHz\n" +
-                              $"Моя память\t{p}Gb (доступно {p1:P0})\n" +
-                              $"Жесткий диск\t{s1*1024:E2} Gb\n" +
-                              $"Тип системы\t{r:F0}-разрядная ОС\n\n\n" +
-                              $"Моя производительность\t{pr:G2} оп/сек\n" +
-                              $"Индекс произв-ти\t{f:F0}\n" +
-                              $"Моя стоимость\t\t{dec:C}");
-
+            int[] NoD = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            Info(NoD);
+            Console.Write("Введите номер месяца:"); int N = int.Parse(Console.ReadLine());
+            Console.Write("Введите номер дня:"); int n = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество дней, которые нужно отсчитать:"); int d = int.Parse(Console.ReadLine());
+            SFiguration(N, n, d, NoD);
             Console.ReadKey();
         }
     }
