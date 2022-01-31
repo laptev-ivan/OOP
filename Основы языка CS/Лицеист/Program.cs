ac15f3ddd729661294f263905c0a01e508d1bda4
Ivan Laptev <chelsea09021999@gmail.com>
2022-01-31 14:48:33 +0300

    Commit

--- Основы языка CS/Лицеист/Program.cs
+++ Основы языка CS/Лицеист/Program.cs
@@ -10,32 +10,35 @@ namespace Лицеист
         class201 = 201,
         class304 = 304
     };
-
-    enum teachers
-    {
-        Иванов,
-        Петрова,
-        Сергеев,
-    };
     struct Lesson
     {
         public classes Classroom;
-        public string Teacher;
-        public string Subject;
-        public int Number;
+        public string[] Teacher;
+        public string[] Group;
+        public string[] Subject;
+        public int[] Number;
     }
     class Program
     {
         static void Main(string[] args)
         {
+            Lesson l;
             StreamReader input = new StreamReader("Z:\\OOP\\Основы языка CS\\Лицеист\\input.txt");
             StreamWriter output = new StreamWriter("Z:\\OOP\\Основы языка CS\\Лицеист\\output.txt");
 
-            int n = int.Parse(input.ReadLine()); // Наборы
-            for (int i = 0; i < n; ++i)          
+            string[] teachers = new string[] { "Иванов", "Петрова", "Сергеев" };
+            string[] subjects = new string[] { "математика", "литература", "английский", "русский" };
+            string[] groups = new string[] { "5.1", "5.2", "5.3" };
+
+            int n = int.Parse(input.ReadLine()); 
+
+            Lesson[,] array = new Lesson[n, groups.Length];
+            for(int i=0; i<n; ++i)
             {
-                
-            }
+                for (int j = 0; j < groups.Length; ++j)
+                {
+                    array[i, ]
+                }
 
             input.Close();
             output.Close();
