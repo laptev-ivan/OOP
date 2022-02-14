642347771d003c6e9fea334a4210e28cca073f70
Ivan Laptev <chelsea09021999@gmail.com>
2022-02-14 11:33:45 +0300

    Commit

--- 0 Практики/12 Практика/Program.cs
+++ 0 Практики/12 Практика/Program.cs
@@ -19,7 +19,7 @@ namespace _12_Практика
             y = Y;
             z = Z;
         }
-        
+
         public void Move(char c, int s)
         {
             if (c == 'x')
@@ -28,11 +28,6 @@ namespace _12_Практика
                 y += s;
             else if (c == 'z')
                 z += s;
-        }
-
-        public void PrintXYZ()
-        {
-            Console.WriteLine($"X:{x}, Y:{y}, Z:{z}");
         }
 
         public double RadiusVector()
@@ -46,25 +41,27 @@ namespace _12_Практика
             x += point2.x;
             y += point2.y;
             z += point2.z;
-            Console.WriteLine($"Сложение двух точек X:{x}, Y:{y}, Z:{z}");
+        }
+
+        public void PrintXYZ()
+        {
+            Console.WriteLine($"X:{x}, Y:{y}, Z:{z}");
         }
     }
 
     class Program
     {
-
-
         static void Main(string[] args)
         {
             Console.WriteLine("Введите 0 — без параметров");
             Console.WriteLine("Введите 1 — с параметрами");
             int ok1 = int.Parse(Console.ReadLine());
-            if (ok1==0)
+            if (ok1 == 0)
             {
                 Point3D point1 = new Point3D();
                 point1.PrintXYZ();
             }
-            else if(ok1==1)
+            else if (ok1 == 1)
             {
                 Console.Write("Введите координату по OX: ");
                 int x1 = int.Parse(Console.ReadLine());
@@ -76,9 +73,9 @@ namespace _12_Практика
                 Console.WriteLine("Введите 0 — не сдвигать");
                 Console.WriteLine("Введите 1 — сдвигать");
                 int ok2 = int.Parse(Console.ReadLine());
-                if(ok2==1)
+                if (ok2 == 1)
                 {
-                    Console.WriteLine("Введите название оси (x, y, z)"); 
+                    Console.WriteLine("Введите название оси (x, y, z)");
                     char c = char.Parse(Console.ReadLine());
                     Console.WriteLine("Расстояние");
                     int s = int.Parse(Console.ReadLine());
@@ -97,8 +94,10 @@ namespace _12_Практика
                 double radius2 = point2.RadiusVector();
                 Console.WriteLine($"Длина радиус вектора второй точки: {radius2:F2}");
                 point1.AddDots(point2);
+                point1.PrintXYZ();
             }
             Console.ReadKey();
         }
     }
 }
+
