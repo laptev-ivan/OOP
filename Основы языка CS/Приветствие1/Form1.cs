7810c9a0e9c228de66eff271df2eafd0a7f7236c
LaptevIvan <laptev.ivan@lit1533.ru>
2022-05-02 18:01:21 +0300

    Commit

---
+++ Основы языка CS/Приветствие1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Приветствие1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if (tb1.Text == "")
+                tb2.Text = "Hello, world!!!";
+            else
+                tb2.Text = "Hello, " + tb1.Text + "!!!";
+        }
+    }
+}
